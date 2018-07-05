9eee4160be00ce28898eea353382ccdcf625f1b9
unyil <1318360+unyil@users.noreply.github.com>
2018-07-05 20:42:26 +0700

    Update to make pool more ready for Aura

--- proxy/config.go
+++ proxy/config.go
@@ -1,10 +1,10 @@
 package proxy
 
 import (
-	"github.com/sammy007/open-ethereum-pool/api"
-	"github.com/sammy007/open-ethereum-pool/payouts"
-	"github.com/sammy007/open-ethereum-pool/policy"
-	"github.com/sammy007/open-ethereum-pool/storage"
+	"github.com/unyil/open-ethereum-pool/api"
+	"github.com/unyil/open-ethereum-pool/payouts"
+	"github.com/unyil/open-ethereum-pool/policy"
+	"github.com/unyil/open-ethereum-pool/storage"
 )
 
 type Config struct {
